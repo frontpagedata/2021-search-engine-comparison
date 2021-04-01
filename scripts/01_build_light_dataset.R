94f90b76c90e44d0cb5033d12e307080504aabf9
Antoine Fabri <antoine.fabri@gmail.com>
2021-04-01 13:22:36 +0200

    add data cleaning explanations

--- scripts/01_build_light_dataset.R
+++ scripts/01_build_light_dataset.R
@@ -3,7 +3,7 @@ library(tidyverse)
 ## clean search_results
 
 search_results <- read_rds(here::here("proc_data/search_results.rds"))
-
+# [1] 7606414      17
 search_results_light <- search_results
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -135,55 +135,21 @@ search_results_light <-
 print(dim(search_results_light)) # [1] 6520886      11
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# remove keywords for which we don't find rank_group from 1 to 30 for
-# every search engine
+# remove unneeded observations with rank > 30
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 search_results_light <-
   search_results_light %>%
-  # group_by(keyword_id, search_engine) %>%
-  # # number of provided ranks in the top 30 for each keyword SE pair
-  # mutate(n = sum(1:30 %in% rank_group)) %>%
-  # ungroup()  %>%
-  # group_by(keyword_id) %>%
-  # # an incomplete keyword is one that doesn't provide 30 top 30 result for any SE
-  # mutate(incomplete = any(n!=30)) %>%
-  # ungroup() %>%
-  # # keep only complete
-  # filter(!incomplete) %>%
-  # select(-n, -incomplete) %>% 
-# # remove unused results
-filter(rank_group <= 30)
+  filter(rank_group <= 30)
 
-print(dim(search_results_light)) # [1] 694680     11 
-# previously # [1] 6324619      11
-
-# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# # remove keywords for which we don't find all 4 search engines
-# #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 
-# search_results_light <-
-#   search_results_light %>%
-#   group_by(keyword_id) %>%
-#   mutate(n = length(unique(search_engine))) %>%
-#   ungroup()
-# 
-# # table(search_results_light$n)
-# #>    1       2       3       4 
-# #> 2357   10289  146732 6354282 
-# 
-# search_results_light <-
-#   search_results_light %>%
-#   filter(n == 4) %>%
-#   select(-n)
-# 
-# print(dim(search_results_light)) # [1] 694680     11 
-# # previously # [1] 6324619      11
+print(dim(search_results_light)) # [1] 6520838      11
 
 search_results_light <-
   search_results_light %>%
   complete(keyword, search_engine, rank_group) %>%
   replace_na(list(url = "missing_url", domain = "missing_domain", monthly_search_volume = "missing_volume"))
+
+print(dim(search_results_light)) # 
 
 write_rds(search_results_light, here::here("proc_data/search_results_light.rds"))
 
